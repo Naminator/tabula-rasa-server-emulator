9976788ef673cb6c27871fd864816f62f476b662
xtremescript <xtremescript@d6b412fb-78a4-b329-1f31-49661cb3b798>
2011-04-23 20:04:58 +0000

    Uploading the source so far

---
+++ TRLoginServer/src/Database/Tables/ServerList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+using TRLoginServer.src.Utils;
+using TRLoginServer.src.Database;
+using TRLoginServer.src.Network.Gameserver;
+
+using MySql.Data.MySqlClient;
+
+
+namespace TRLoginServer.src.Database.Tables
+{
+    class ServerList
+    {
+        static ServerList instance = null;
+        static readonly object padlock = new object();
+
+        public static ServerList Instance
+        {
+            get { lock (padlock) { if (instance == null)instance = new ServerList(); return instance; } }
+        }
+
+        public SortedList<string, GameServerInfo> GameServerList { get; set; }
+
+        public ServerList()
+        {
+            GameServerList = new SortedList<string, GameServerInfo>();
+        }
+
+        public void Initialize()
+        {
+            Logger.WriteLog("Loading the server list...", Logger.LogType.Initialize);
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM servers", DatabaseFactory.Instance.GetDBConnection());
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                byte id = dr.GetByte(0);
+                short port = dr.GetInt16(3);
+                string name = dr.GetString(1);
+                IPAddress serverAddr = IPAddress.Parse(dr.GetString(2));
+                ushort onlineUsers = dr.GetUInt16(7);
+                uint dateCreated = dr.GetUInt32(4);
+                bool developerOnly = Convert.ToBoolean(dr.GetByte(6));
+
+                GameServerList.Add(name, new GameServerInfo(id, name, serverAddr, port, onlineUsers, dateCreated, developerOnly));
+            }
+
+            dr.Close();
+            cmd.Dispose();
+        }
+
+        public void ReloadServerList()
+        {
+            //@todo - Make this thing someday
+        }
+    }
+}
---
+++ TRLoginServer/src/Utils/ConfigFile.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TRLoginServer.src.Utils
+{
+    public class ConfigFile
+    {
+        private FileInfo File;
+        private SortedList<string, SortedList<string, string>> _topics;
+
+        public ConfigFile(string path)
+        {
+            File = new FileInfo(path);
+            _topics = new SortedList<string, SortedList<string, string>>();
+            Reload();
+        }
+
+        public void Reload()
+        {
+            if (!File.Exists)
+            {
+                Logger.WriteLog("Unable to load the configuration file at " + File.FullName, Logger.LogType.Error);
+                return;
+            }
+
+            StreamReader SR = new StreamReader(File.FullName);
+            string curTopic = "";
+            while (!SR.EndOfStream)
+            {
+                string line = SR.ReadLine();
+
+                //Few checks
+                if (line.Length == 0) { continue; }
+                if (line.StartsWith(";")) { continue; }
+
+                //This is a new topic
+                if (line.StartsWith("["))
+                {
+                    curTopic = line.Replace("[", "").Replace("]", "");
+                    Logger.WriteLog("Topic added: " + curTopic, Logger.LogType.Initialize);
+
+                    _topics.Add(curTopic, new SortedList<string, string>());
+                    continue;
+                }
+
+                string value = "";
+
+                for (int i = 1; i < line.Trim().Split('=').Length; i++)
+                {
+                    if (i == line.Trim().Split('=').Length - 1)
+                    {
+                        value += line.Trim().Split('=')[i];
+                    }
+                    else
+                    {
+                        value += line.Trim().Split('=')[i] + "=";
+                    }
+                }
+                _topics[curTopic].Add(line.Trim().Split('=')[0], value);
+
+
+            }
+        }
+
+        public string getProperty(string Topic, string Prop, string DefaultValue)
+        {
+            string ret = null;
+            try
+            {
+                ret = _topics[Topic][Prop];
+            }
+            catch
+            {
+                ret = null;
+                Logger.WriteLog("Trying to load unexistant property: "  + Prop + " in " + Topic, Logger.LogType.Error);
+            }
+
+            return (string.IsNullOrEmpty(ret) ? DefaultValue : ret);
+        }
+    }
+}
